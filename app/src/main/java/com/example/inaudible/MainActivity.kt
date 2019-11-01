7ace1bbd1157959a24e1380bd5370629b4497f0d
Jason Huh <jaeseok.huh.0@gmail.com>
2019-11-01 18:22:49 +0900

    Beautiufy the code

--- app/src/main/java/com/example/inaudible/MainActivity.kt
+++ app/src/main/java/com/example/inaudible/MainActivity.kt
@@ -3,38 +3,31 @@ package com.example.inaudible
 import android.Manifest
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.R.attr.start
 import android.content.pm.PackageManager
 import android.media.MediaRecorder
 import android.media.AudioRecord
-import androidx.core.app.ComponentActivity.ExtraData
-import androidx.core.content.ContextCompat.getSystemService
-import android.icu.lang.UCharacter.GraphemeClusterBreak.T
-import android.media.AudioFormat.ENCODING_PCM_16BIT
-import android.media.AudioFormat.CHANNEL_IN_MONO
 import android.media.AudioFormat
 import android.util.Log
 import android.widget.TextView
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import java.lang.Math.abs
-import java.lang.Math.random
-import java.sql.Timestamp
-import kotlin.random.Random
-
 
 
 class MainActivity : AppCompatActivity() {
     private var recorder: AudioRecord? = null
+
     private val RECORDER_SAMPLERATE = 44100
     private val RECORDER_CHANNELS = AudioFormat.CHANNEL_IN_MONO
     private val RECORDER_AUDIO_ENCODING = AudioFormat.ENCODING_PCM_16BIT
+
     private var recordingThread: Thread? = null
+
     private var BufferSize = 1024
     private val FFT_N = 512
-    private val fsmFreq: IntArray = intArrayOf(19000, 20000, 21000) // 17000 for silence
-    private var prvCode = 0
-    private var lastCode = 0
+    private val fsmFreq: IntArray = intArrayOf(19000, 20000, 21000)
+    private var prvCode = 0 // the previous code that is received right before
+    private var lastCode = 0 // the last code that is consumed by the parser
 
     private var lastTimestamp: Long = 0
 
@@ -43,8 +36,11 @@ class MainActivity : AppCompatActivity() {
 
     private val MY_PERMISSIONS_AUDIO_RECORDING = 0x0000001
     private val MSG_PERMITION_DENIED = "Permission Request Denied"
+    private val RATIO_THRESHOLD = 8.0
 
+    private val CODE_SILENCE = 3
 
+    /*
     private fun getVisualizedString(freqData: DoubleArray): String {
         var s: String = ""
         for (i in 0 until FFT_N / 2 / 4) {
@@ -56,55 +52,50 @@ class MainActivity : AppCompatActivity() {
             s += "[%d-%d) %f\n".format(i * 4, i * 4 + 4, sum / 4.0)
         }
         return s
-    }
+    } */
 
     private fun getCode(freqData: DoubleArray): Int {
-        var highFreqData = freqData.slice(216..FFT_N / 2).toDoubleArray()
+        val highFreqData = freqData.slice(216..FFT_N / 2).toDoubleArray()
 
-
-        var idx = highFreqData.indices.maxBy { highFreqData[it] }!! + 216
-
+        val idx = highFreqData.indices.maxBy { highFreqData[it] }!! + 216
         val freq: Int = idx * (RECORDER_SAMPLERATE / 2) / (FFT_N / 2)
+
+        // FSM code in {0, 1, 2}
+        // clockwise (modular increment) means 0 (false)
+        // counter-clockwise (modular decrement) means 1 (true)
+
         val code = fsmFreq.indices.minBy { abs(fsmFreq[it] - freq) }!!
         val ratio = freqData[idx]/ highFreqData.average()
 
-
-
-        if (ratio >= 8.0) {
+        if (ratio >= RATIO_THRESHOLD) {
             Log.d("ratio", "%f".format(ratio))
             Log.d("code", code.toString())
             return code
         }
         else {
-            // Log.i("code", code.toString())
-
-            return 3
+            return CODE_SILENCE
         }
 
     }
 
     private fun updateAudioData() {
-
-        var codes : MutableList<Int> = ArrayList()
+        val codes : MutableList<Int> = ArrayList()
 
         while(true) {
-            var sData = ShortArray(BufferSize)
+            val sData = ShortArray(BufferSize)
             var code : Int
 
-
             recorder!!.read(sData, 0, BufferSize)
-            var freqData : DoubleArray = FFT(n=FFT_N).getFreqSpectrumFromShort(sData)
+            val freqData : DoubleArray = FFT(n=FFT_N).getFreqSpectrumFromShort(sData)
 
             code = getCode(freqData)
 
-            if (code == 3) {
+            if (code == CODE_SILENCE) {
                 val currentTimestamp = System.currentTimeMillis()
-                if (prvCode == 3) {
+                if (prvCode == CODE_SILENCE) {
                     if (currentTimestamp - lastTimestamp >= TIMEOUT_MILLIS) {
                         displayText = ""
                         codes.clear()
-
-
                     }
                 } else {
                     lastTimestamp = currentTimestamp
@@ -112,12 +103,12 @@ class MainActivity : AppCompatActivity() {
             } else if (0 <= code && code <= 2) {
                 if (code == prvCode) continue
 
-                codes.add(code);
+                codes.add(code)
             }
 
             if (codes.size >= 8) {
-                var sum : Int = 0
-                var p : Int = 1
+                var sum = 0
+                var p = 1
                 for (i in 7 downTo 0) {
                     val t = (if (i==0) lastCode else codes[i-1])
                     if (codes[i] == (t -1 + 3)%3) {
@@ -138,7 +129,7 @@ class MainActivity : AppCompatActivity() {
             }
             prvCode = code
 
-            changeText1(displayText);
+            changeText1(displayText)
         }
     }
 
@@ -210,7 +201,5 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         requestAudioPermission()
-
     }
-
 }
